60d03a0d3ec82c31bb59dc8791520e3c398fd677
wealhtheow@gmail.com <robinson.a@wustl.edu>
2019-06-04 16:07:07 -0400

    comprehension

---
+++ python/codeex2.py
@@ -0,0 +1,12 @@
+# numbers = [50, 52, 55, 60, 74]
+# data = []
+# for number in numbers:
+#        data.append(number-10)
+
+#print(data)
+
+
+numbers = [50, 52, 55, 60, 74]
+data = [number - 10 for number in numbers]
+
+print(data)
