37757c8bdbd5edbffb093bfab776f08fa0d8011f
wealhtheow@gmail.com <robinson.a@wustl.edu>
2019-06-04 15:08:38 -0400

    computating

---
+++ python/codeex1.py
@@ -0,0 +1,5 @@
+length=457
+words=250
+pieces=100
+total_words=length*words
+print(total_words/pieces)
---
+++ python/myaddress.py
@@ -0,0 +1,4 @@
+my_address = {"address1":"Campus Box 1061", "address2":"One Brookings Way", "city":"St. Louis", "state":"MO", "zip":"63310"}
+print(my_address["address1"])
+print(my_address["address2"])
+print(my_address["city"]+", "+my_address["state"]+" "+my_address["zip"])
